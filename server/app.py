77b0ca71f5cc864566df4eedac6519adfac3130d
sujin <sujinsr2457@gmail.com>
2025-06-20 19:17:18 +0100

    updated production urls

--- server/app.py
+++ server/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://statistical-data-visualisation-plat.vercel.app/"}}, supports_credentials=True, allow_headers=["Content-Type", "Authorization"], methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+CORS(app, resources={r"/*": {"origins": "https://statistical-data-visualisation-plat.vercel.app"}}, supports_credentials=True, allow_headers=["Content-Type", "Authorization"], methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 
 from api.upload import upload_bp
 
 app.register_blueprint(upload_bp, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
