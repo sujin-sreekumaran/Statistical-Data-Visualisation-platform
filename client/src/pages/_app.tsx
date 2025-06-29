9f91928630a274c03e464a45e17701fb8c41d163
sujin <sujinsr2457@gmail.com>
2025-06-29 16:50:48 +0100

    Remove SessionProvider from _app.tsx
    The SessionProvider wrapper was removed from the custom App component, so session context from next-auth is no longer provided to pages. This may be in preparation for a different authentication approach or to simplify the app structure.

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,8 @@
-import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import "../styles/globals.css"; // Make sure this line is present
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
